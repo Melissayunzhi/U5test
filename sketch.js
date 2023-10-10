d3019b5b2194348e32014f136f6f55ad64d8290c
Yunzhi Li <melissaliyunzhi@gmail.com>
2023-10-10 15:42:52 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ let offset;                 // Offset for panning
 let audioContextStarted = false;
 
 function setup() {
-  var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(windowWidth, windowHeight);
   //canvas.parent("container");
   gridSize = createVector(width / CELL_SIZE, height / CELL_SIZE);
   
