1e6de1de857e2db46eaff486c62887fe1c498b66
Yunzhi Li <melissaliyunzhi@gmail.com>
2023-10-23 19:21:33 +0100

    Update drawingtest.js

--- drawingtest.js
+++ drawingtest.js
@@ -133,6 +133,8 @@ function draw() {
    image(video, 0, 0, width, height);
 
   // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+
 
   // applyZoomAndOffset();
   displayGrid();
